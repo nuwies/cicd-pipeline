b5e906447ebb4ad4491df9007106629633324858
jimmytsang42 <jimmytsang42@gmail.com>
2024-09-22 22:19:07 -0700

    added additional home redirects, tested deleting all categories and all questions, tested no categories no questions

--- WEB-INF/classes/PlayServlet.java
+++ WEB-INF/classes/PlayServlet.java
@@ -2,6 +2,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
--- WEB-INF/classes/QuestionUploadServlet.java
+++ WEB-INF/classes/QuestionUploadServlet.java
@@ -3,6 +3,7 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.http.Part;
+
 import java.sql.*;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,150 +12,151 @@ import java.nio.file.*;
 @MultipartConfig
 public class QuestionUploadServlet extends DbConnectionServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    HttpSession session = request.getSession(false);
-    if (session == null) {
-      response.setStatus(HttpServletResponse.SC_FOUND);
-      response.sendRedirect("login");
-      return;
-    }
-
-    String username = (String) session.getAttribute("username");
-    String userType = null;
-
-    try (Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-        PreparedStatement ps = con
-            .prepareStatement("SELECT user_type FROM users WHERE username = ?")) {
-      ps.setString(1, username);
-      try (ResultSet rs = ps.executeQuery()) {
-        if (rs.next()) {
-          userType = rs.getString("user_type");
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.setStatus(HttpServletResponse.SC_FOUND);
+            response.sendRedirect("login");
+            return;
         }
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
+
+        String username = (String) session.getAttribute("username");
+        String userType = null;
+
+        try (Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+             PreparedStatement ps = con
+                     .prepareStatement("SELECT user_type FROM users WHERE username = ?")) {
+            ps.setString(1, username);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    userType = rs.getString("user_type");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (!"admin".equalsIgnoreCase(userType)) {
+            response.sendRedirect("main");
+            return;
+        }
+
+        Connection con = null;
+        ResultSet result = null;
+        String categorySelect = "";
+        int numCategories = 0;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception ex) {
+            System.out.println("Message: " + ex.getMessage());
+            return;
+        }
+
+        try {
+            con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            Statement stmt = con.createStatement();
+            result = stmt.executeQuery("SELECT * FROM categories");
+            while (result.next()) {
+                numCategories++;
+                String category = result.getString("name");
+                String categoryID = result.getString("id");
+                System.out.println(category);
+                System.out.println(categoryID);
+                categorySelect += "<option value='" + categoryID + "'>" + category + "</option>";
+            }
+            if (numCategories == 0)
+                response.sendRedirect("no-categories.html");
+        } catch (SQLException ex) {
+            while (ex != null) {
+                System.out.println("Message: " + ex.getMessage());
+                System.out.println("SQLState: " + ex.getSQLState());
+                System.out.println("ErrorCode: " + ex.getErrorCode());
+                ex = ex.getNextException();
+                System.out.println("");
+            }
+        }
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>"
+                + "<head><title>Create A Quiz Question</title></head>"
+                + "<body><h1>Create A Quiz Question</h1><br>"
+                + "<a href='main'>Back to Main Page</a><br><br>"
+                + "<form method='POST' action='upload-question' enctype='multipart/form-data'>"
+                + "<label for='category'>Category</label><select id='category' name='category' required>"
+                + categorySelect + "</select><br>"
+                + "<label for='question'>Question:</label><input type='text' id='question' name='question' maxlength='256' required><br>"
+                + "<label for='correct-answer'>Correct Answer:</label><input type='text' id='correct-answer' name='correct-answer' maxlength='256' required><br>"
+                + "<label for='wrong-answer1'>Wrong Answer 1:</label><input type='text' id='wrong-answer1' name='wrong-answer1' maxlength='256' required><br>"
+                + "<label for='wrong-answer2'>Wrong Answer 2 (Optional):</label><input type='text' id='wrong-answer2' name='wrong-answer2' maxlength='256'><br>"
+                + "<label for='wrong-answer3'>Wrong Answer 3 (Optional):</label><input type='text' id='wrong-answer3' name='wrong-answer3' maxlength='256'><br>"
+                + "<input type='File' id='file' name='filename'><br>"
+                + "<input type='submit' value='Submit'>"
+                + "</form>"
+                + "</body></html>");
     }
 
-    if (!"admin".equalsIgnoreCase(userType)) {
-      response.sendRedirect("main");
-      return;
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Part filePart = request.getPart("filename");
+        String categoryID = request.getParameter("category");
+        String question = request.getParameter("question");
+        String correctAnswer = request.getParameter("correct-answer");
+        String wrongAnswer1 = request.getParameter("wrong-answer1");
+        String wrongAnswer2 = request.getParameter("wrong-answer2");
+        String wrongAnswer3 = request.getParameter("wrong-answer3");
+        String fileName = filePart.getSubmittedFileName();
+        String filePath = "";
+        if (!fileName.trim().isEmpty())
+            filePath = System.getProperty("catalina.base") + "/webapps/comp3940-assignment1/media/" + fileName;
+
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception ex) {
+            System.out.println("Message: " + ex.getMessage());
+            return;
+        }
+
+        try {
+            con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            PreparedStatement preparedStatement = con
+                    .prepareStatement(
+                            "INSERT INTO questions (category, question, correct_answer, wrong_answer_1, wrong_answer_2, wrong_answer_3, content_path) VALUES (?,?,?,?,?,?,?)");
+
+            preparedStatement.setInt(1, Integer.valueOf(categoryID));
+            preparedStatement.setString(2, question);
+            preparedStatement.setString(3, correctAnswer);
+            preparedStatement.setString(4, wrongAnswer1);
+            preparedStatement.setString(5, wrongAnswer2);
+            preparedStatement.setString(6, wrongAnswer3);
+            if (!fileName.trim().isEmpty())
+                preparedStatement.setString(7, "media/" + fileName);
+            else
+                preparedStatement.setString(7, fileName);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException ex) {
+            while (ex != null) {
+                System.out.println("Uploading error!");
+                System.out.println("Message: " + ex.getMessage());
+                System.out.println("SQLState: " + ex.getSQLState());
+                System.out.println("ErrorCode: " + ex.getErrorCode());
+                ex = ex.getNextException();
+                System.out.println("");
+            }
+        }
+
+        // Save file in server images directory
+        try {
+            filePart.write(filePath);
+        } catch (Exception e) {
+            System.out.println("No file was selected!");
+        }
+
+        response.sendRedirect("upload-success.html");
     }
-
-    Connection con = null;
-    ResultSet result = null;
-    String categorySelect = "";
-    int numCategories = 0;
-
-    try {
-      Class.forName("com.mysql.cj.jdbc.Driver");
-    } catch (Exception ex) {
-      System.out.println("Message: " + ex.getMessage());
-      return;
-    }
-
-    try {
-      con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-      Statement stmt = con.createStatement();
-      result = stmt.executeQuery("SELECT * FROM categories");
-      while (result.next()) {
-        numCategories++;
-        String category = result.getString("name");
-        String categoryID = result.getString("id");
-        System.out.println(category);
-        System.out.println(categoryID);
-        categorySelect += "<option value='" + categoryID + "'>" + category + "</option>";
-      }
-      if (numCategories == 0)
-        response.sendRedirect("no-categories.html");
-    } catch (SQLException ex) {
-      while (ex != null) {
-        System.out.println("Message: " + ex.getMessage());
-        System.out.println("SQLState: " + ex.getSQLState());
-        System.out.println("ErrorCode: " + ex.getErrorCode());
-        ex = ex.getNextException();
-        System.out.println("");
-      }
-    }
-
-    response.setContentType("text/html");
-    PrintWriter out = response.getWriter();
-    out.println("<!DOCTYPE html>"
-        + "<head><title>Create A Quiz Question</title></head>"
-        + "<body><h1>Create A Quiz Question</h1><br>"
-        + "<form method='POST' action='upload-question' enctype='multipart/form-data'>"
-        + "<label for='category'>Category</label><select id='category' name='category' required>"
-        + categorySelect + "</select><br>"
-        + "<label for='question'>Question:</label><input type='text' id='question' name='question' maxlength='256' required><br>"
-        + "<label for='correct-answer'>Correct Answer:</label><input type='text' id='correct-answer' name='correct-answer' maxlength='256' required><br>"
-        + "<label for='wrong-answer1'>Wrong Answer 1:</label><input type='text' id='wrong-answer1' name='wrong-answer1' maxlength='256' required><br>"
-        + "<label for='wrong-answer2'>Wrong Answer 2 (Optional):</label><input type='text' id='wrong-answer2' name='wrong-answer2' maxlength='256'><br>"
-        + "<label for='wrong-answer3'>Wrong Answer 3 (Optional):</label><input type='text' id='wrong-answer3' name='wrong-answer3' maxlength='256'><br>"
-        + "<input type='File' id='file' name='filename'><br>"
-        + "<input type='submit' value='Submit'>"
-        + "</form>"
-        + "</body></html>");
-  }
-
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    Part filePart = request.getPart("filename");
-    String categoryID = request.getParameter("category");
-    String question = request.getParameter("question");
-    String correctAnswer = request.getParameter("correct-answer");
-    String wrongAnswer1 = request.getParameter("wrong-answer1");
-    String wrongAnswer2 = request.getParameter("wrong-answer2");
-    String wrongAnswer3 = request.getParameter("wrong-answer3");
-    String fileName = filePart.getSubmittedFileName();
-    String filePath = "";
-    if (!fileName.trim().isEmpty())
-      filePath = System.getProperty("catalina.base") + "/webapps/comp3940-assignment1/media/" + fileName;
-
-    Connection con = null;
-    try {
-      Class.forName("com.mysql.cj.jdbc.Driver");
-    } catch (Exception ex) {
-      System.out.println("Message: " + ex.getMessage());
-      return;
-    }
-
-    try {
-      con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-      PreparedStatement preparedStatement = con
-          .prepareStatement(
-              "INSERT INTO questions (category, question, correct_answer, wrong_answer_1, wrong_answer_2, wrong_answer_3, content_path) VALUES (?,?,?,?,?,?,?)");
-
-      preparedStatement.setInt(1, Integer.valueOf(categoryID));
-      preparedStatement.setString(2, question);
-      preparedStatement.setString(3, correctAnswer);
-      preparedStatement.setString(4, wrongAnswer1);
-      preparedStatement.setString(5, wrongAnswer2);
-      preparedStatement.setString(6, wrongAnswer3);
-      if (!fileName.trim().isEmpty())
-        preparedStatement.setString(7, "media/" + fileName);
-      else
-        preparedStatement.setString(7, fileName);
-      preparedStatement.executeUpdate();
-      preparedStatement.close();
-    } catch (SQLException ex) {
-      while (ex != null) {
-        System.out.println("Uploading error!");
-        System.out.println("Message: " + ex.getMessage());
-        System.out.println("SQLState: " + ex.getSQLState());
-        System.out.println("ErrorCode: " + ex.getErrorCode());
-        ex = ex.getNextException();
-        System.out.println("");
-      }
-    }
-
-    // Save file in server images directory
-    try {
-      filePart.write(filePath);
-    } catch (Exception e) {
-      System.out.println("No file was selected!");
-    }
-
-    response.sendRedirect("upload-success.html");
-  }
 }
